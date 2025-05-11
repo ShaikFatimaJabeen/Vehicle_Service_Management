0aa8358108948dd3eace23dfd775395fdcf82ffb
ShaikFatimaJabeen <shaikfatimajabeen@gmail.com>
2025-05-11 23:18:35 +0530

    Add files via upload

---
+++ Vehicle_Service_Management/src/test/java/com/aits/VechicleServiceManagement/VechicleServiceManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aits.VechicleServiceManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VechicleServiceManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
